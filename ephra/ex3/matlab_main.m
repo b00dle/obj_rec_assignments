c7bcd3ca9fcd64e1607f4e1745664ab489a84d1b
Ephraim Schott <senu8384@in.uni-weimar.de>
2017-05-29 19:30:19 +0200

    added function deg2rad

--- ephra/ex3/matlab_main.m
+++ ephra/ex3/matlab_main.m
@@ -37,6 +37,10 @@ function [H, o_idx_vec, p_idx_vec] = hough_voting_array(img_thresh)
   end
 end
 
+function y = deg2rad(x)
+ y = x * pi/180;
+end
+
 function [H, o_idx_vec, p_idx_vec] = hough_ephra(img_thresh)
   [nr, nc] = size(img_thresh);
   p_max = int32(sqrt(nr^2 + nc^2));
@@ -51,7 +55,8 @@ function [H, o_idx_vec, p_idx_vec] = hough_ephra(img_thresh)
       if img_thresh(y, x)
         for o_i = 1:180
           o = o_i - 91;
-          p = x*cos(o) + y*sin(o);
+          rad_o = deg2rad(o);
+          p = x*cos(rad_o) + y*sin(rad_o);
           p_i = int32(p)+p_max;
           H(p_i, o_i) = H(p_i, o_i) + 1;
         end          
@@ -109,6 +114,81 @@ function [filter_x, filter_y, r] = GoG_filter(sigma)
     filter_y = filter_x';
 end
 
+function [r, c, hnew] = houghpeaks_uni(h, numpeaks, threshold, nhood)
+    %HOUGHPEAKS Detect peaks in Hough transform.
+    %   [R, C, HNEW] = HOUGHPEAKS(H, NUMPEAKS, THRESHOLD, NHOOD) detects
+    %   peaks in the Hough transform matrix H.  NUMPEAKS specifies the
+    %   maximum number of peak locations to look for.  Values of H below
+    %   THRESHOLD will not be considered to be peaks.  NHOOD is a
+    %   two-element vector specifying the size of the suppression
+    %   neighborhood.  This is the neighborhood around each peak that is
+    %   set to zero after the peak is identified.  The elements of NHOOD
+    %   must be positive, odd integers.  R and C are the row and column
+    %   coordinates of the identified peaks.  HNEW is the Hough transform
+    %   with peak neighborhood suppressed. 
+    %
+    %   If NHOOD is omitted, it defaults to the smallest odd values >=
+    %   size(H)/50.  If THRESHOLD is omitted, it defaults to
+    %   0.5*max(H(:)).  If NUMPEAKS is omitted, it defaults to 1. 
+
+    %   Copyright 2002-2004 R. C. Gonzalez, R. E. Woods, & S. L. Eddins
+    %   Digital Image Processing Using MATLAB, Prentice-Hall, 2004
+    %   $Revision: 1.5 $  $Date: 2003/11/21 13:34:50 $
+    disp('HELLOO');
+    if nargin < 4
+       nhood = size(h)/50;
+       % Make sure the neighborhood size is odd.
+       nhood = max(2*ceil(nhood/2) + 1, 1);
+    end
+    if nargin < 3
+       threshold = 0.5 * max(h(:));
+    end
+    if nargin < 2
+       numpeaks = 1;
+    end
+
+    done = false;
+    hnew = h; r = []; c = [];
+    while ~done
+       [p, q] = find(hnew == max(hnew(:)));
+       p = p(1); q = q(1);
+       if hnew(p, q) >= threshold
+          r(end + 1) = p; c(end + 1) = q;
+
+          % Suppress this maximum and its close neighbors.
+          p1 = p - (nhood(1) - 1)/2; p2 = p + (nhood(1) - 1)/2;
+          q1 = q - (nhood(2) - 1)/2; q2 = q + (nhood(2) - 1)/2;
+          [pp, qq] = ndgrid(p1:p2,q1:q2);
+          pp = pp(:); qq = qq(:);
+
+          % Throw away neighbor coordinates that are out of bounds in
+          % the rho direction.
+          badrho = find((pp < 1) | (pp > size(h, 1)));
+          pp(badrho) = []; qq(badrho) = [];
+
+          % For coordinates that are out of bounds in the theta
+          % direction, we want to consider that H is antisymmetric
+          % along the rho axis for theta = +/- 90 degrees.
+          theta_too_low = find(qq < 1);
+          qq(theta_too_low) = size(h, 2) + qq(theta_too_low);
+          pp(theta_too_low) = size(h, 1) - pp(theta_too_low) + 1;
+          theta_too_high = find(qq > size(h, 2));
+          qq(theta_too_high) = qq(theta_too_high) - size(h, 2);
+          pp(theta_too_high) = size(h, 1) - pp(theta_too_high) + 1;
+
+          % Convert to linear indices to zero out all the values.
+          hnew(sub2ind(size(hnew), pp, qq)) = 0;
+
+          done = length(r) == numpeaks;
+       else
+          done = true;
+       end
+    end
+end
+
+
+
+
 
 
 function [result] = main( filepath )
@@ -130,20 +210,12 @@ function [result] = main( filepath )
     figure('Name','Binary Image with Tresh=0.07');
     imshow(img_bw);
 
-    [H,theta,rho] = hough(img_bw);
-    disp(max(H));
-    disp('1 Theta');
-    disp(size(theta ));
-    disp('1 rho');
-    disp(size(rho));
-    %[H,theta,rho] = hough_voting_array(img_bw);
     [H,theta,rho] = hough_ephra(img_bw);
-    disp(max(H));
     disp('2 Theta');
     disp(size(theta ));
     disp('2 rho');
     disp(size(rho));
-    % disp(H(1:10,1:10));
+
     
    
 
@@ -153,9 +225,11 @@ function [result] = main( filepath )
     axis equal;
     axis image;
     
-    h_peaks = houghpeaks(H);
+    h_peaks = houghpeaks(H,1);
     figure('Name', 'Houghpeaks Result');
     imshow(h_peaks);
+    disp(size(h_peaks));
+    %disp(h_peaks);
     
     lines = houghlines(img_bw,theta, rho, h_peaks);
 
